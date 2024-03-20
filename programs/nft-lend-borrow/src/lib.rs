2bc462276b368359a7c199d7f5ed0e5a1fb75820
vinstudent33 <164144008+vinstudent33@users.noreply.github.com>
2024-03-20 14:19:37 +0700

    Update lib.rs

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("Awx9Tenq8vK4DLfYTKKhsFf5k4YJvCyPFppuu21Ra2DD");
 
 #[program]
 pub mod nft_lend_borrow {
